2417e621b7a74c0e7c7a9473a744ba616953288f
esperanza-Q <suruna1026@gmail.com>
2025-05-13 06:01:23 +0900

    feat : naverlogin

--- user/models.py
+++ user/models.py
@@ -6,8 +6,9 @@ from django.db import models
 class CustomUser(AbstractUser):
     nickname=models.CharField(max_length=20, blank=False, null= False)
     kakao_id = models.CharField(max_length=255, unique=True, null= True)
+    naver_id = models.CharField(max_length=255, unique=True, null= True)
     # phone_number = models.CharField(max_length=15, blank=True, null=True)
-    email = models.EmailField(unique=True, null=True, blank=True)
+    email = models.EmailField(unique=False, null=True, blank=True)
     
     groups = models.ManyToManyField(Group, related_name="customuser_set", blank=True)
     user_permissions = models.ManyToManyField(Permission, related_name="customuser_permissions_set", blank=True)
--- user/views.py
+++ user/views.py
@@ -81,7 +81,7 @@ def kakao_callback_view(request):
     response = requests.post(kakao_token_url, data=data)
     token_data = response.json()
 
-    print(f"Response from Kakao token API: {token_data}")   # 토큰 제대로 받고 있나 확인
+    # print(f"Response from Kakao token API: {token_data}")   # 토큰 제대로 받고 있나 확인
 
     if 'access_token' not in token_data:
         return HttpResponse('Failed to get access token', status=400)
@@ -97,15 +97,15 @@ def kakao_callback_view(request):
     user_info_response = requests.get(kakao_user_info_url, headers=headers)
     user_info = user_info_response.json()
 
-    print(user_info)    # 정보 제대로 받고 있나 확인
+    # print(user_info)    # 정보 제대로 받고 있나 확인
 
     # 사용자 정보에서 필요한 데이터 추출
     kakao_id = user_info['id']
     nickname = user_info.get('properties', {}).get('nickname', '')
     email = user_info.get('kakao_account', {}).get('email', '')
 
-    print(f"Kakao ID: {kakao_id}")  # kakao ID 제대로 받고 있나 확인
-    print(f"Nickname: {nickname}")  # nickname 제대로 받고 있나 확인
+    # print(f"Kakao ID: {kakao_id}")  # kakao ID 제대로 받고 있나 확인
+    # print(f"Nickname: {nickname}")  # nickname 제대로 받고 있나 확인
 
     if not email:   # 이메일을 받으려면 비즈 어쩌구를 신청해야함(내 생각에는 정식 개발용인 것 같음) -> 결론적으로 현재는 email을 못 받으니까 이렇게 None 값으로 넣어줌
         email = None
@@ -117,16 +117,89 @@ def kakao_callback_view(request):
         defaults={
             'username': nickname,  # 기본적으로 카카오에서 제공하는 닉네임을 사용자 이름으로 사용
             'nickname': nickname,
-            'email' : email,   # 닉네임도 저장
+            'email' : email,   # 내 생각에는 계속 인증 오류 발생하는 게, email을 빈칸으로 둬서 그런 것 같기도 하고... 일단 이거 작성하니까 로그인 됐음
         }
     )
 
     # 만약 사용자가 이미 존재하면 정보 업데이트
     if not created:
         # user.username = nickname # 그런데 이렇게 하면 로그인할 때마다 username이랑 nickname이 원상복구 됨.... 그래서 일단은 주석으로 막아뒀는데, 괜찮은지는 아직 테스트 못 해봄
-        # user.nickname = nickname
+        # user.nickname = nickname  # 괜찮음!!!!!! 오예~~!!!!!! 
         user.save()
 
     login(request, user)
 
     return redirect('user:home')
+
+
+import urllib.parse
+
+def naver_login_view(request):
+    client_id = conf.NAVER_CLIENT_ID
+    redirect_uri = urllib.parse.quote(conf.NAVER_REDIRECT_URI)  # URL 인코딩
+    state = 'random_state_string'  # CSRF 보호용 임의의 문자열
+    auth_url = f"https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id={client_id}&redirect_uri={redirect_uri}&state={state}"
+    return redirect(auth_url)
+
+def naver_callback_view(request):
+    code = request.GET.get('code')
+    state = request.GET.get('state')
+
+     # 디버그용 출력
+    print("네이버 인증 코드:", code)  # 인증 코드 출력
+    print("상태 코드:", state)  # 상태 출력
+
+    # 네이버 액세스 토큰 요청
+    token_url = 'https://nid.naver.com/oauth2.0/token'
+    client_id = conf.NAVER_CLIENT_ID
+    client_secret = conf.NAVER_CLIENT_SECRET
+    redirect_uri = conf.NAVER_REDIRECT_URI
+
+    token_data = {
+        'grant_type': 'authorization_code',
+        'client_id': client_id,
+        'client_secret': client_secret,
+        'code': code,
+        'state': state,
+    }
+
+    response = requests.post(token_url, data=token_data)
+    print("액세스 토큰 요청 응답:", response.json())  # 액세스 토큰 응답 출력
+    token_info = response.json()
+
+    # 액세스 토큰 얻기
+    access_token = token_info.get('access_token')
+
+    if access_token:
+        print("액세스 토큰:", access_token)
+    else:
+        print("액세스 토큰 오류 발생:", token_info)
+
+    # 네이버 API로 사용자 정보 요청
+    user_info_url = 'https://openapi.naver.com/v1/nid/me'
+    headers = {
+        'Authorization': f'Bearer {access_token}'
+    }
+
+    user_info_response = requests.get(user_info_url, headers=headers)
+
+    print("사용자 정보 요청 응답:", user_info_response.json())
+    user_info = user_info_response.json()
+
+    # 사용자 정보에서 필요한 데이터 가져오기
+    naver_id = user_info.get('response', {}).get('id')
+    nickname = user_info.get('response', {}).get('nickname')
+    email = user_info.get('response', {}).get('email')
+
+    user, created = CustomUser.objects.get_or_create(naver_id=naver_id, defaults={'username':email, 'email': email, 'nickname': nickname})
+
+    if not created:
+        # user.username = nickname # 그런데 이렇게 하면 로그인할 때마다 username이랑 nickname이 원상복구 됨.... 그래서 일단은 주석으로 막아뒀는데, 괜찮은지는 아직 테스트 못 해봄
+        # user.nickname = nickname  # 괜찮음!!!!!! 오예~~!!!!!! 
+        user.save()
+
+    login(request, user)
+    # 로그인 처리 (예: 사용자 등록 및 세션 처리)
+    # 여기서 필요한 로직을 추가하여 사용자를 등록하거나 세션에 로그인 상태를 저장합니다.
+
+    return redirect('user:home')  # 로그인 후 리다이렉트
